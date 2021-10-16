4031696ea58ee32b76fb8c22035108482280415b
Adam Wojdylo <adam.wojdylo1234@gmail.com>
2021-10-16 16:15:59 +0200

    Chapter6 Implement Binay Tree with PHP arrays

--- Chapter6BinaryTree.php
+++ Chapter6BinaryTree.php
@@ -60,3 +60,48 @@ $semiFinal2->addChildren($quarterFinal3, $quarterFinal4);
 $final->addChildren($semiFinal1, $semiFinal2);
 
 $tree->traverse($tree->root);
+
+echo "\nImplementacja drzewa binarnego za pomocą tablicy PHP\n";
+/*
+    Przy ustaleniu ,że w tym drzewie binarnym węzły są uporządkowane jako korzeń - 0 , lewy dziecko 1 prawe dziecko 2 
+    dziecko 1-go lewe 3 prawe 4 dziecko 2-go lew 5 prawe 6 
+    dziecko 3-go lewe 7 prawe 8 dziecko 4-go lewe 9 prawe 10 
+    dziecko 5-go lewe 11 
+
+    Dzięki temu można zauważyć prosty wzór 
+    jeśli i jest numerem węzła wówczas:
+    Lewy węzeł = 2 X i + 1
+    Prawe węzeł 2 X (i + 1)
+*/
+$nodes = [];
+$nodes[] = "Final";
+$nodes[] = "Polfinal 1";
+$nodes[] = "Polfinal 2";
+$nodes[] = "Cwiercfinal 1";
+$nodes[] = "Cwiercfinal 2";
+$nodes[] = "Cwiercfinal 3";
+$nodes[] = "Cwiercfinal 4";
+
+
+class BinaryTreeAr {
+    public $nodes = [];
+
+    public function __construct(Array $nodes)
+    {
+        $this->nodes = $nodes; 
+    }
+
+    public function traverse(int $num = 0, int $level = 0) {
+
+        if(isset($this->nodes[$num])){
+            echo str_repeat("-", $level);
+            echo $this->nodes[$num] . "\n";
+
+            $this->traverse(2 * $num + 1, $level + 1);
+            $this->traverse(2 * ($num + 1), $level + 1);
+        }
+    }
+}
+
+    $tree = new BinaryTreeAr($nodes);
+    $tree->traverse(0);
