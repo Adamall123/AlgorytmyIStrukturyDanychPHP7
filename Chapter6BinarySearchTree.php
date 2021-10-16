c16b7ce54d996b67461afbd7275ef5eb8abe0d1f
Adam Wojdylo <adam.wojdylo1234@gmail.com>
2021-10-16 20:11:23 +0200

    Chapter6 traverse paths with 3 different ways

--- Chapter6BinarySearchTree.php
+++ Chapter6BinarySearchTree.php
@@ -149,7 +149,40 @@ class BST {
                 $this->traverse($node->right);
         }
     }
-
+    public function traverseTypes(Node $node, string $type = "in-order") {
+        switch($type){
+            case "in-order":
+                $this->inOrder($node);
+                break;
+            case "pre-order":
+                $this->preOrder($node);
+                break;
+            case "post-order":
+                $this->postOrder($node);
+                break;
+        }
+    }
+    public function preOrder(Node $node){
+        if($node) {
+            echo $node->data . " ";
+            if($node->left) $this->traverseTypes($node->left);
+            if($node->right) $this->traverseTypes($node->right);
+        }
+    }
+    public function inOrder(Node $node){
+        if($node) {
+            if($node->left) $this->traverseTypes($node->left);
+            echo $node->data . " ";
+            if($node->right) $this->traverseTypes($node->right);
+        }
+    }
+    public function postOrder(Node $node){
+        if($node) {
+            if($node->left) $this->traverseTypes($node->left);
+            if($node->right) $this->traverseTypes($node->right);
+            echo $node->data . " ";
+        }
+    }
     public function search(int $data) {
         if($this->isEmpty()){
             return FALSE; 
@@ -188,11 +221,44 @@ $tree->insert(13);
 $tree->insert(36);
 
 
-$tree->traverse($tree->root);
+// $tree->traverse($tree->root);
 
-echo  $tree->search(7) ? "Znaleziono\n" : "Nieznaleziono\n";
-echo  $tree->search(36) ? "Znaleziono\n" : "Nieznaleziono\n";
+// echo  $tree->search(7) ? "Znaleziono\n" : "Nieznaleziono\n";
+// echo  $tree->search(36) ? "Znaleziono\n" : "Nieznaleziono\n";
+// echo "\n";
+
+// $tree->remove(6);
+// $tree->traverse($tree->root);
+
+$tree->traverseTypes($tree->root, 'pre-order');
 echo "\n";
+$tree->traverseTypes($tree->root, 'in-order');
+echo "\n";
+$tree->traverseTypes($tree->root, 'post-order');
 
-$tree->remove(6);
-$tree->traverse($tree->root);
+/*
+    Przechodzenie przez drzewo oznacza operację polegającą na odwiedzeniu
+    każdego węzła danego drzewa. W zależności od sposobu wykonania tej 
+    operacji można wymienić trzy różne sposoby przechodzenia przez 
+    drzewo. 
+
+    PRZECHODZENIE BEZPOŚREDNIE
+
+    W bezpośrednim przechodzeniu przez drzewo (ang. in-order traversal)
+    najpierw odwiedza się lewy węzeł, następnie korzeń,a dopiero na końcu
+    prawy węzeł. To działanie jest powtarzane rekurencyjnie dla każdego
+    węzła. Lewy węzeł przechowuje mniejszą wartość niż wartość, która
+    jest przechowywana przez korzeń, zaś prawy węzeł zawiera wartość większą
+    niż ta ostatnia. W wyniku tego, stosując przechodzenie bezpośrednie, 
+    otrzymujemy uporządkowaną listę wartości. 
+    1. Przejdź przez lewe poddrzewo, rekurencyjnie wywołując funkcję 
+    bezpośredniego przechodzenia.
+    2. Wyświetl dane przechowywane przez korzeń (lub bieżący węzeł)
+    3. Przejdź prawe poddrzewo, rekurencyjnie wywołując funkcję bezpośredniego
+    przechodzenia. 
+
+    PRZECHODZENIE Z WYPRZEDZENIEM
+
+    PRZECHODZENIE Z OPÓŹNIENIEM 
+*/
+
