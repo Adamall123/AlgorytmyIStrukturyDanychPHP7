7916caf277abdf91ce0d22fd368e421cd0117972
Adam Wojdylo <adam.wojdylo1234@gmail.com>
2021-11-07 21:56:01 +0100

    Chapter11 add memoization

--- Chapter11Memoization.php
+++ Chapter11Memoization.php
@@ -12,23 +12,41 @@
 */
 
 $startTime = microtime();
-
+$fibCashe = [];
 $count = 0;
 
-function fibonacci(int $n): int {
+function fibonacciMemoized(int $n): int {
+    global $fibCashe;
     global $count;
     $count++;
-    if($n == 0){
-        return 1;
-    } else if ($n == 1){
+    if($n == 0 || $n == 1){
         return 1;
     } else{
-        return fibonacci($n-1) + fibonacci($n - 2);
+
+        if(isset($fibCashe[$n - 1])) 
+        {
+            $tmp  = $fibCashe[$n - 1];
+        }else {
+            $tmp = fibonacciMemoized($n -1);
+            $fibCashe[$n - 1] = $tmp; 
+        }
+
+        if(isset($fibCashe[$n - 2])) 
+        {
+            $tmp1  = $fibCashe[$n - 2];
+        }else {
+            $tmp1 = fibonacciMemoized($n - 2);
+            $fibCashe[$n - 1] = $tmp1; 
+        }
+        return $tmp + $tmp1;
     }
 }
 
-echo fibonacci(30) . "\n";
+echo fibonacciMemoized(30) . "\n";
 echo "Funckja wywołania: " . $count . "\n";
 $endTime = microtime();
 $totalTime = $endTime - $startTime;
 echo "Czas = " . $totalTime . "\n";
+
+// W kodzie funkcji sprawdzamy czy liczba , której szukamy jest w tablicy, jeśli tak to nie obliczamy jej ponownie
+// a wykorzystujemy zapisaną wcześniej wartość Czas wywołań i wykonania sięfunkcji jest dużo szybszy
